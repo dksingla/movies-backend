fcb8bff3a1b8eeccdfde359de13d49cbbaed5318
Deepak Singla <deepak.stoic@gmail.com>
2024-08-08 11:05:21 +0530

    pushed

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,7 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin:[ 'http://localhost:3000','https://movies-frontend-six.vercel.app/'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
+  app.enableCors();
   await app.listen(3001);
 }
 bootstrap();
