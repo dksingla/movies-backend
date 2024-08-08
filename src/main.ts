e0816b1e66c0e053b94d829796a8e802273032b2
Deepak Singla <deepak.stoic@gmail.com>
2024-08-08 11:58:51 +0530

    changed

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000', 'https://movies-frontend-six.vercel.app'], 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+  app.enableCors(corsOptions);
   await app.listen(3001);
 }
 bootstrap();
